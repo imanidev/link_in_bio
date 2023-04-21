ecb242030c9c3c3a7ef8f8f4d9bda45b566c5d0a
imani.devs <imaniroberts.dev@gmail.com>
2023-04-21 18:56:33 -0400

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const name = "Hi, I'm Imani";
 const element = document.getElementById("Hi, I'm Imani");
+const speed = 100; // time delay in milliseconds between each character
 
 function typeWriter(text, el) {
-    let i = 0;
-    const speed = 100; // time delay in milliseconds between each character
-    function type() {
-        if (i < text.length) {
-            el.textContent += text.charAt(i);
-            i++;
-            setTimeout(type, speed);
-        }
-    }
-    type();
+	//el is the element to print the text to (h1)
+	//text is the text to print
+	let i = 0;
+
+	function type() {
+		if (i < text.length) {
+			el.textContent += text.charAt(i); // add next character to h1 element text
+			i++; // increment i by 1 for next char in text
+			setTimeout(type, speed); // call type function after speed delay (100ms)
+		}
+	}
+	type();
 }
 
 typeWriter(name, element);
